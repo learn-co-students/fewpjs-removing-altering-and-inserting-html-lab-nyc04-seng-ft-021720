087193bb611857e7a477d10fff5d6f768f88b104
kev-kev <kevin.clark93@gmail.com>
2020-03-31 16:27:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("#main");
+main.remove();
+
+const h1 = document.createElement("h1");
+h1.id = "victory";
+document.body.append(h1);
+
+let newHeader = document.querySelector("h1#victory");
+newHeader.textContent = "me is the champion"
